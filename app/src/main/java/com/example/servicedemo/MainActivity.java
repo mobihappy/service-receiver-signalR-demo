f10dbe1c32a9484f4a509d56762b4e520d0ee6ff
mobihappy <spctn.ht@gmail.com>
2019-03-15 09:58:16 +0700

    init commit

---
+++ app/src/main/java/com/example/servicedemo/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.servicedemo;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private MyService mService;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view){
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent);
+            }
+        });
+    }
+
+    public void bindService(View view){
+        Intent intent = new Intent(this, MyService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyService.LocalBinder binder = (MyService.LocalBinder) service;
+            mService = binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(intent);
+    }
+}
---
+++ app/src/main/java/com/example/servicedemo/MyService.java
@@ -0,0 +1,95 @@
+package com.example.servicedemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+import java.util.concurrent.ExecutionException;
+
+import microsoft.aspnet.signalr.client.Platform;
+import microsoft.aspnet.signalr.client.SignalRFuture;
+import microsoft.aspnet.signalr.client.http.android.AndroidPlatformComponent;
+import microsoft.aspnet.signalr.client.hubs.HubConnection;
+import microsoft.aspnet.signalr.client.hubs.HubProxy;
+import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler2;
+import microsoft.aspnet.signalr.client.transport.ClientTransport;
+import microsoft.aspnet.signalr.client.transport.ServerSentEventsTransport;
+
+public class MyService extends Service {
+    private final IBinder iBinder = new LocalBinder();
+    private HubConnection mHubConnection;
+    private HubProxy mHubProxy;
+    private Handler mHandler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mHandler = new Handler(Looper.getMainLooper());
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//            }
+//        }, 3000, 10000);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startSignalR();
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        startSignalR();
+        return iBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mHubConnection.stop();
+        Log.d("onDestroy", "destroy");
+        Intent intent = new Intent();
+        intent.setAction("restartService");
+        intent.setClass(this, MyReceiver.class);
+        sendBroadcast(intent);
+    }
+
+    public class LocalBinder extends Binder{
+        MyService getService(){
+            return MyService.this;
+        }
+    }
+
+    private void startSignalR(){
+        Platform.loadPlatformComponent(new AndroidPlatformComponent());
+
+        String serverUrl = "http://icichtc.ewave.vn:4902";
+        mHubConnection = new HubConnection(serverUrl);
+        String SERVER_HUB_CHAT = "ChatHub";
+        mHubProxy = mHubConnection.createHubProxy(SERVER_HUB_CHAT);
+
+        ClientTransport clientTransport = new ServerSentEventsTransport(mHubConnection.getLogger());
+        SignalRFuture<Void> signalRFuture = mHubConnection.start(clientTransport);
+
+        try {
+            signalRFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            Log.d("SimpleSignalR", e.toString());
+            return;
+        }
+
+        String CLIENT_METHOD_BROADAST_MESSAGE = "broadcastMessage";
+        mHubProxy.on(CLIENT_METHOD_BROADAST_MESSAGE,
+                new SubscriptionHandler2<String, String>() {
+                    @Override
+                    public void run(final String name, final String msg) {
+                        new NotificationHelper(MyService.this).createNotification(name, msg);
+                    }
+                }, String.class,String.class);
+    }
+}
